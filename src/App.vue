56b1b3bb832de73e0881f97908523bab2875173f
George <george@DESKTOP-RHD02MA.localdomain>
2021-05-06 00:06:05 +0200

    messed up

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+   <nav>
+      <span><router-link class="nav-logo" to="/work">GM </router-link></span>
+
+      <ul class="nav-list">
+
+        <li> <router-link class="nav-about" to="/about"> About</router-link></li>
+        <li> <router-link class="nav-work" to="/work"> Work</router-link></li>
+      
+      </ul>
+    </nav>
+
+  <router-view v-slot="{ Component }">
+    
+    <component :is="Component" />
+  </router-view>
 </template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+ font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: auto;
+ 
 }
 
-#nav {
-  padding: 30px;
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+ 
+  margin-bottom: 20px;
+  margin:  0 5%;
+  padding-bottom: 20px;
+  border-bottom: 2px double #a9c0ef;
 }
 
-#nav a {
+.nav-logo{
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 72px;
+  text-transform: uppercase;
+  color: #1e2d48;
+  margin: 0;
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-list, .nav-about, .nav-work {
+  color: #1e2d48;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 20px;
+  align-items: base;
+  list-style-type: none;
+  font-weight: 200;
+  font-size: 36px;
+  padding-bottom: 1rem;
+  text-decoration: none;
+  text-transform: lowercase;
+  margin: 0;
+  padding-top: 5px;
 }
+.nav-about:hover, .nav-work:hover{
+ 
+  text-decoration-line: underline;
+  transition-duration: 500ms;
+  
+  color:burlywood;
+}
+
+
 </style>
