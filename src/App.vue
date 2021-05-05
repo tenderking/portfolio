6246b83b0aaf2f4dfd1950a4966ad8a6412447c0
George Mushore <gmu003@gmail.com>
2021-05-05 11:25:03 +0200

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <About />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './components/about.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    About
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+ font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: auto;
+ 
 }
 </style>
